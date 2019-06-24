6f44627af945f7332be89dbd8f67ce9237ad5e1c
WenFan-SW <52166707+WenFan-SW@users.noreply.github.com>
2019-06-24 17:09:45 +0100

    Part 2 First Attempt

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,35 @@
 let n = 1;
+let word = "";
 while (n <= 100) {
-    if (n % 15 === 0) {
-        console.log('FizzBuzz');
-    } else if (n % 3 === 0) {
-        console.log('Fizz');
+    if (n % 3 === 0) {
+        word += 'Fizz';
     } else if (n % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(n);
+        word += 'Buzz';
+    } else if (n % 7 === 0) {
+        word += 'Bang';
+    } else if (n % 11 === 0) {
+        console.log('Bong');
+        break ;
+    } else if (n % 13 === 0) {
+        if (word.includes('Buzz')) {
+            word = word.replace('Buzz', 'FezzBuzz');
+            break ;
+        } else if (word.includes('Bang')) {
+            word = word.replace('Bang', 'FezzBang');
+            break ;
+        } else {
+            word += 'Fezz');
+        }
     }
+    // } else if (n % 17 === 0) {
+    //     word
+    // }
+    }
+    if (n === "") {
+        console.log(n);
+    } else {
+        console.log(word);
+    }
+
     n++;
 }
