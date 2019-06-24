915d28b7ac0ffa90ac8f48b5ce2c1f6339596a2f
WenFan-SW <52166707+WenFan-SW@users.noreply.github.com>
2019-06-24 17:31:20 +0100

    Part 2 Last Monday Attempt

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,35 +1,32 @@
 let n = 1;
-let word = "";
-while (n <= 100) {
+while (n <= 225) {
+    let word = "";
     if (n % 3 === 0) {
         word += 'Fizz';
-    } else if (n % 5 === 0) {
+    } if (n % 5 === 0) {
         word += 'Buzz';
-    } else if (n % 7 === 0) {
+    } if (n % 7 === 0) {
         word += 'Bang';
-    } else if (n % 11 === 0) {
-        console.log('Bong');
-        break ;
-    } else if (n % 13 === 0) {
+    } if (n % 13 === 0) {
         if (word.includes('Buzz')) {
             word = word.replace('Buzz', 'FezzBuzz');
-            break ;
         } else if (word.includes('Bang')) {
             word = word.replace('Bang', 'FezzBang');
-            break ;
         } else {
             word += 'Fezz';
         }
     }
-    // } else if (n % 17 === 0) {
+    // if (n % 17 === 0) {
     //     word
     // }
+    if (n % 11 === 0) {
+        word = 'Bong';
     }
-    if (n === "") {
+
+    if (word === "") {
         console.log(n);
     } else {
         console.log(word);
     }
-
     n++;
 }
