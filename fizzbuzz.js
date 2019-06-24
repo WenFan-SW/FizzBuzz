01678c337037a3e3f0f215c49a15a968f7ca4a5b
WenFan-SW <52166707+WenFan-SW@users.noreply.github.com>
2019-06-24 16:27:23 +0100

    Second commit

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,7 +5,7 @@ while (n <= 100) {
 
     if (n % 15 == 0) {
         console.log('FizzBuzz');
-}   else if (n % 5 == 0) {
+}   else if (n % 3 == 0) {
         console.log('Buzz');
 }   else if (n % 5 == 0) {
             console.log('Buzz')
