5b59c9a90e0a6ccd5b7851a2f5d5b8e4dc17dec6
WenFan-SW <52166707+WenFan-SW@users.noreply.github.com>
2019-06-24 16:45:58 +0100

    Part 1 Complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,13 @@
-
-
-var n = 1;
+let n = 1;
 while (n <= 100) {
-
-    if (n % 15 == 0) {
+    if (n % 15 === 0) {
         console.log('FizzBuzz');
-}   else if (n % 3 == 0) {
+    } else if (n % 3 === 0) {
         console.log('Fizz');
-}   else if (n % 5 == 0) {
+    } else if (n % 5 === 0) {
         console.log('Buzz');
-}   else {
+    } else {
         console.log(n);
-}
+    }
     n++;
 }
