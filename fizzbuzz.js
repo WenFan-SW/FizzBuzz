a30810330d281b08c408c04f5e1fbed18d3ac8b3
WenFan-SW <52166707+WenFan-SW@users.noreply.github.com>
2019-06-25 10:46:29 +0100

    Part 2 Finished

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,27 +1,33 @@
-let n = 1;
-while (n <= 255) {
-    let word = "";
+function reverse(word) {
+    let reversed = '';
+    for (let i = 0; i < word.length; i += 4) {
+        reversed += word.substr(-(i + 4), 4);
+    }
+    return reversed;
+}
+
+function fezzAdd(word) {
+    let firstB = word.indexOf('B');
+    word = word.slice(0, firstB) + 'Fezz' + word.slice(firstB);
+    return word;
+}
+
+function printFizzBuzz(n) {
+    let word = '';
     if (n % 3 === 0) {
         word += 'Fizz';
-    } if (n % 5 === 0) {
+    }
+    if (n % 5 === 0) {
         word += 'Buzz';
-    } if (n % 7 === 0) {
+    }
+    if (n % 7 === 0) {
         word += 'Bang';
-    } if (n % 13 === 0) {
-        if (word.includes('Buzz')) {
-            word = word.replace('Buzz', 'FezzBuzz');
-        } else if (word.includes('Bang')) {
-            word = word.replace('Bang', 'FezzBang');
-        } else {
-            word += 'Fezz';
-        }
+    }
+    if (n % 13 === 0) {
+        word = fezzAdd(word);
     }
     if (n % 17 === 0) {
-        let word1 = "";
-        for (i = 0; i < word.length; i += 4) {
-            word1 += word.substr(-(i+4), 4);
-        }
-        word = word1;
+        word = reverse(word);
     }
     if (n % 11 === 0) {
         word = 'Bong';
@@ -32,5 +38,11 @@ while (n <= 255) {
     } else {
         console.log(word);
     }
+}
+
+let n = 1;
+// const n = require('Enter a number');
+while (n <= 255) {
+    printFizzBuzz(n);
     n++;
 }
